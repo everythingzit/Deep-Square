131a558de1e248fd954e6654ce69469b53d77cd0
Hendrik Tebeng <hendriktebeng@gmail.com>
2026-02-15 11:45:35 -0500

    Implement frontend piece click functionality

--- Frontend/src/pages/Game.jsx
+++ Frontend/src/pages/Game.jsx
@@ -5,12 +5,12 @@ import { useState, useCallback } from "react"
 function Game() {
   const [game, setGame] = useState(new Chess())
   const [position, setPosition] = useState(game.fen())
+  const [origin, setOrigin] = useState(null)
+  const [options, setOptions] = useState({})
 
   const onDrop = useCallback((moveData) => {
     try {
         const gameCopy = new Chess(game.fen())
-
-        console.log(moveData)
 
         const move = gameCopy.move({
             from: moveData.sourceSquare,
@@ -31,9 +31,67 @@ function Game() {
     }
   }, [game]);
 
-  const onClick = useCallback((square, piece) => {
-    
-  }, [game])
+  const getOptions = useCallback((square) => {
+    const moves = game.moves({
+        square: square,
+        verbose: true
+    })
+
+    if (moves.length === 0) {
+        setOptions([])
+        return false
+    }
+
+    const newSquares = {}
+
+    setOptions(newSquares)
+
+    return true
+  })
+
+  const onClick = useCallback((squareData) => {
+    if (!origin && squareData.piece) {
+        const hasMoves = getOptions(squareData.square)
+        if (hasMoves) {
+            setOrigin(squareData.square)
+        }
+
+        return
+    }
+
+    const moves = game.moves({
+        square: origin,
+        verbose: true
+    })
+
+    const foundMove = moves.find(m => m.from === origin && m.to === squareData.square)
+
+    if (!foundMove) {
+        const hasMoves = getOptions(squareData.square)
+        setOrigin(hasMoves ? squareData.square : null)
+
+        return
+    }
+
+    try {
+        game.move({
+            from: origin,
+            to: squareData.square,
+            promotion: "q"
+        })
+    } catch (e) {
+        const hasMoves = getOptions(squareData.square)
+        if (hasMoves) {
+            setOrigin(squareData.square)
+        }
+
+        return
+    }
+
+    setPosition(game.fen())
+    setOrigin(null)
+    setOptions({})
+  }, [origin])
 
   const boardOptions = {
     position: position,
