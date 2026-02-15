ae06c51ce684a177635c0f8ac172c160ce27d1d4
Hendrik Tebeng <hendriktebeng@gmail.com>
2026-02-14 22:02:59 -0500

    Initialize React application

---
+++ Engine/board.py
@@ -0,0 +1,11 @@
+import chess
+
+class Board:
+    def __init__(self):
+        self.board = chess.Board()
+
+    def get_fen(self):
+        return self.board.fen()
+    
+    def set_fen(self, fen: str):
+        self.board = Board(fen)
